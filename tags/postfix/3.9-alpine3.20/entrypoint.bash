dbbe87b16e97b06f882160e4b9a4ad8e39db5501
Jaroslav Petras <niekto@niekde.sk>
2024-10-08 03:40:13 +0200

    deploy new config only if there is none

--- tags/postfix/3.9-alpine3.20/entrypoint.bash
+++ tags/postfix/3.9-alpine3.20/entrypoint.bash
@@ -6,12 +6,15 @@ if [[ ! -v MAIL_CONFIG ]]; then
   exit 100
 fi
 
-tar -C /etc/postfix -cf - . | tar -C /usr/local/etc/postfix -xf -
+if [[ ! -f ${MAIL_CONFIG}/main.cf ]]; then
+  echo "INFO: No configuration detected, making default one."
+  tar -C /etc/postfix -cf - . | tar -C /usr/local/etc/postfix -xf -
 
-postconf -n -c /etc/postfix > "${MAIL_CONFIG}/main.cf"
-postconf -e "config_directory = ${MAIL_CONFIG}" 
-postconf -e "meta_directory = ${MAIL_CONFIG}" 
-postconf -e "sample_directory = ${MAIL_CONFIG}" 
-postconf -e "maillog_file = /dev/stdout"
+  postconf -n -c /etc/postfix > "${MAIL_CONFIG}/main.cf"
+  postconf -e "config_directory = ${MAIL_CONFIG}" 
+  postconf -e "meta_directory = ${MAIL_CONFIG}" 
+  postconf -e "sample_directory = ${MAIL_CONFIG}" 
+  postconf -e "maillog_file = /dev/stdout"
+fi
 
 exec /usr/sbin/postfix start-fg 
